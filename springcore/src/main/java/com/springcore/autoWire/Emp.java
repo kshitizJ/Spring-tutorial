ac7cc683ef730b1569a5aa7c13ff1ec5979607bb
kshitizJ <coolkshitiz786@gmail.com>
2021-06-07 21:35:52 +0530

    autowiring using xml file

---
+++ springcore/src/main/java/com/springcore/autoWire/Emp.java
@@ -0,0 +1,29 @@
+package com.springcore.autoWire;
+
+public class Emp {
+    private Address address;
+
+    public Emp(Address address) {
+        super();
+        this.address = address;
+    }
+
+    public Emp() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Emp [address=" + address + "]";
+    }
+
+}
---
+++ springcore/src/main/java/com/springcore/autoWire/Test.java
@@ -0,0 +1,18 @@
+package com.springcore.autoWire;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/autoWire/autowireconfig.xml");
+
+		// with typecast
+		// Emp emp = (Emp) context.getBean("employee");
+
+		// if we dont want to type cast then we can do the following
+		Emp emp = context.getBean("employee", Emp.class);
+
+		System.out.println(emp);
+	}
+}
